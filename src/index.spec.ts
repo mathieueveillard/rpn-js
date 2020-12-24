95bb332201f498c81e570b1ce880e9f31a172b77
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-12-24 10:44:10 +0100

    A possible solution

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,33 @@
-import { systemUnderTest } from ".";
+import { rpn } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
-  });
+it("should compute a simple addition", function () {
+  expect(rpn([0, 1, "+"])).toEqual(1);
+});
+
+it("should compute a simple addition", function () {
+  expect(rpn([1, 1, "+"])).toEqual(2);
+});
+
+it("should compute a simple substraction", function () {
+  expect(rpn([0, 1, "-"])).toEqual(-1);
+});
+
+it("should compute a simple multiplication", function () {
+  expect(rpn([1, 2, "*"])).toEqual(2);
+});
+
+it("should compute a simple division", function () {
+  expect(rpn([1, 2, "/"])).toEqual(0.5);
+});
+
+it("should compute 1 2 + 2 *", function () {
+  expect(rpn([1, 2, "+", 2, "*"])).toEqual(6);
+});
+
+it("should compute 2 1 2 + * (control)", function () {
+  expect(rpn([2, 1, 2, "+", "*"])).toEqual(6);
+});
+
+it("should compute 1 2 + -", function () {
+  expect(rpn([1, 2, "+", "-"])).toEqual(-3);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
-export function systemUnderTest(): boolean {
-  return true;
+type Operator = "+" | "-" | "*" | "/";
+
+type Operand = number;
+
+type Instructions = (Operator | Operand)[];
+
+export function rpn(o: Instructions): number {
+  const [result] = internal([...o].reverse());
+  return result;
+}
+
+function internal([i, ...r1]: Instructions): [Operand, Instructions] {
+  if (isOperand(i)) {
+    return [i, r1];
+  }
+
+  const [b, r2] = internal(r1);
+  if (isUnaryMinusOperation(i, r2)) {
+    return [-b, []];
+  }
+
+  const [a, r3] = internal(r2);
+  switch (i) {
+    case "+":
+      return [a + b, r3];
+    case "-":
+      return [a - b, r3];
+    case "*":
+      return [a * b, r3];
+    case "/":
+      return [a / b, r3];
+  }
+}
+
+function isOperand(instruction: Operator | Operand): instruction is Operand {
+  return !isNaN(instruction as Operand);
+}
+
+function isUnaryMinusOperation(operator: Operator, rest: Instructions): boolean {
+  return operator === "-" && rest.length === 0;
 }
